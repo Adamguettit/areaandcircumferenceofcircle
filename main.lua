15f22b4bc4c847b0461603bb3ac51645e8976731
adam <adam.guettit@ocsbstudent.ca>
2018-03-01 14:47:59 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,57 @@
+-----------------------------------------------------------------------------------------
+
+--
+
+-- 
+
+-- 
+
+-----------------------------------------------------------------------------------------
+
+local radiousTextField = native.newTextField( display.contentCenterX, display.contentCenterY + 200, 450, 75 )
+
+radiousTextField.id = "radious textField"
+
+local areaOfCircleTextField = display.newText( "area", display.contentCenterX, display.contentCenterY - 200, native.systemFont, 75 )
+
+areaOfCircleTextField.id = "area textField"
+
+local circumferenceTextField = display.newText( "circumference", display.contentCenterX, display.contentCenterY - 300, native.systemFont, 75 )
+
+circumferenceTextField.id = "circumference textField"
+
+local calculateButton = display.newImageRect( "./assets/sprites/enterButton.png", 406, 157 )
+calculateButton.x = display.contentCenterX
+calculateButton.y = display.contentCenterY
+
+calculateButton.id = "calculate button"
+
+ 
+
+local function calculateButtonTouch( event )
+
+    -- this function calculates the area and circumference of a circle given the radious 
+
+ 	local radious
+
+    local areaOfCircle
+
+    local circumference
+
+	radious = radiousTextField.text
+	circumference = circumferenceTextField.text
+	areaOfcircle = areaOfcircleTextField
+	areaOfcircle = radious^2 * math.pi
+	circumference = 2 * math.pi * radious
+	-- print( areaOfSquare )
+
+    areaOfCircleTextField.text = "The area is " .. areaOfcircle
+    circumferenceTextField.text = "the circumference is " .. circumference
+
+	return true
+
+end
+
+
+
+calculateButton:addEventListener( "touch", calculateButtonTouch )
